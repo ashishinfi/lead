94ef117e8b778f0712a0adf4d851204de0b9d105
ashishinfi <ashish.jha@infidigit.com>
2024-03-04 19:29:33 +0530

    vpdated req and update

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -30,10 +30,12 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-#*$75ccfs9i4=lsgn=ovr_&i@gdx6vt_ej$nrmronj_ejkrkxh'
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+DEBUG = False
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
